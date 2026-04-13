d4f2a60293b98a0f495045f94d9e9c60cc1f8088
Ashwini <ashwinikhandare606@gmail.com>
2026-04-13 23:58:34 +0530

    Initial Commit

---
+++ AIreviewer/src/test/java/com/Application/AIreviewer/AIreviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Application.AIreviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIreviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
